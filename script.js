df4427ef1a7abf780ca74d56a4d5418bd38bf99e
AHMED HOSSAM <124696607+AhmedHossam911@users.noreply.github.com>
2023-11-10 18:04:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 // submenu
 {
-const nav = document.querySelector('nav');
-const subMenu = document.querySelector('.sub-menu');
+  const nav = document.querySelector('nav');
+  const subMenu = document.querySelector('.sub-menu');
 
-nav.addEventListener('mouseover', () => {
+  nav.addEventListener('mouseover', () => {
     subMenu.style.display = 'block';
-});
+  });
 
-document.addEventListener('mouseover', (event) => {
+  document.addEventListener('mouseover', (event) => {
     if (!nav.contains(event.target)) {
-        subMenu.style.display = 'none';
+      subMenu.style.display = 'none';
     }
-});
+  });
 
-document.addEventListener('click', (event) => {
+  document.addEventListener('click', (event) => {
     if (!nav.contains(event.target)) {
-        subMenu.style.display = 'none';
+      subMenu.style.display = 'none';
     }
-});
+  });
 }
 
 // back to top
 {
-    // Get a reference to the button
-var scrollToTopButton = document.getElementById("scroll-to-top-button");
+  // Get a reference to the button
+  var scrollToTopButton = document.getElementById("scroll-to-top-button");
 
-// Show or hide the button based on the user's scroll position
-window.addEventListener("scroll", function() {
+  // Show or hide the button based on the user's scroll position
+  window.addEventListener("scroll", function () {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopButton.style.display = "block";
+      scrollToTopButton.style.display = "block";
     } else {
-        scrollToTopButton.style.display = "none";
+      scrollToTopButton.style.display = "none";
     }
-});
+  });
 
-// Scroll to the top when the button is clicked
-scrollToTopButton.addEventListener("click", function() {
+  // Scroll to the top when the button is clicked
+  scrollToTopButton.addEventListener("click", function () {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-});
+  });
 
 }
 
 // sidebar
 {
-function toggleSidebar() {
+  function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.right === '0px') {
       sidebar.style.right = '-250px';
@@ -52,12 +52,12 @@ function toggleSidebar() {
       sidebar.style.right = '0px';
     }
   }
-  
+
   function closeSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.style.right = '-250px';
   }
-  
+
   function handleSidebarHover(enter) {
     const sidebar = document.getElementById('sidebar');
     if (enter) {
@@ -69,25 +69,45 @@ function toggleSidebar() {
   function hideSidebarOnOutsideClick(event) {
     const sidebar = document.getElementById('sidebar');
     const toggleButton = document.getElementById('toggleButton');
-  
+
     if (!sidebar.contains(event.target) && event.target !== toggleButton) {
       closeSidebar();
     }
   }
-  
-  document.addEventListener('click', hideSidebarOnOutsideClick); 
+
+  document.addEventListener('click', hideSidebarOnOutsideClick);
 }
-        
+
 // popup Announcement
 {
-  document.addEventListener('DOMContentLoaded', function() {
-    // Display the popup when the page is loaded
-    document.getElementById('popupContainer').style.display = 'flex';
-});
+  document.addEventListener('DOMContentLoaded', function () {
+    // Check if 24 hours have passed since the last visit
+    if (!localStorage.getItem('popupLastShown') || has24HoursPassed()) {
+      // Display the popup when the page is loaded or 24 hours have passed
+      document.getElementById('popupContainer').style.display = 'flex';
+      // Save the current time in localStorage
+      localStorage.setItem('popupLastShown', new Date().getTime().toString());
+    }
+  });
 
-function closePopup() {
+  function has24HoursPassed() {
+    // Get the last time the popup was shown from localStorage
+    var lastShownTime = localStorage.getItem('popupLastShown');
+    if (!lastShownTime) {
+      return true; // Show the popup if it has never been shown before
+    }
+
+    // Calculate the time difference in milliseconds
+    var currentTime = new Date().getTime();
+    var timeDifference = currentTime - parseInt(lastShownTime);
+
+    // Check if 24 hours have passed (24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
+    return timeDifference > 24 * 60 * 60 * 1000;
+  }
+
+  function closePopup() {
     document.getElementById('popupContainer').style.display = 'none';
-}
+  }
 
 }
 
